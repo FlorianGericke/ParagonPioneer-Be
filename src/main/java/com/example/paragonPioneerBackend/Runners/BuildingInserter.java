c1c0a5b1720ee0b3a6a4ef69f4c276d646446d32
fGericke <florian.gericke@endava.com>
2024-01-04 17:38:58 +0100

    add population capacity to Building Inserter

--- src/main/java/com/example/paragonPioneerBackend/Runners/BuildingInserter.java
+++ src/main/java/com/example/paragonPioneerBackend/Runners/BuildingInserter.java
@@ -1,6 +1,7 @@
 package com.example.paragonPioneerBackend.Runners;
 
-import com.example.paragonPioneerBackend.Entity.Building;
+import com.example.paragonPioneerBackend.Entity.PopulationBuilding;
+import com.example.paragonPioneerBackend.Entity.ProductionBuilding;
 import com.example.paragonPioneerBackend.Repository.BuildingRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
@@ -16,7 +17,6 @@ public class BuildingInserter{
             new Inserter("Lumberjack", ""),
             new Inserter("Forester", ""),
             new Inserter("Warehouse I", ""),
-            new Inserter("Pioneer's Hut", ""),
             new Inserter("Well", ""),
             new Inserter("Kontor I", "Limited to one per island"),
             new Inserter("Fisherman", ""),
@@ -26,8 +26,14 @@ public class BuildingInserter{
     };
 
     public void run() {
+        repository.save(PopulationBuilding.builder().name("Pioneer's Hut").capacity(10).remarks("").build());
+        repository.save(PopulationBuilding.builder().name("Colonist's House").capacity(15).remarks("").build());
+        repository.save(PopulationBuilding.builder().name("Townsmen's House").capacity(20).remarks("").build());
+        repository.save(PopulationBuilding.builder().name("Merchant's Mansion").capacity(25).remarks("").build());
+        repository.save(PopulationBuilding.builder().name("Paragon's Residence").capacity(30).remarks("").build());
+
         for (Inserter insert : inserts) {
-            repository.save(Building.builder().name(insert.name).remarks(insert.remarks).build());
+            repository.save(ProductionBuilding.builder().name(insert.name).remarks(insert.remarks).build());
         }
     }
 }
