2b5a8e7efcf7f4fefe6e97f8721d5f4288cd309f
fGericke <florian.gericke@endava.com>
2024-01-05 17:14:35 +0100

    rearrange execution order for the new Relation between production buildings and recipes

--- src/main/java/com/example/paragonPioneerBackend/Runners/InsertRunner.java
+++ src/main/java/com/example/paragonPioneerBackend/Runners/InsertRunner.java
@@ -1,5 +1,6 @@
 package com.example.paragonPioneerBackend.Runners;
 
+import com.example.paragonPioneerBackend.Repository.RecipeRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
@@ -8,19 +9,21 @@ import org.springframework.stereotype.Component;
 @Component
 @RequiredArgsConstructor
 public class InsertRunner implements ApplicationRunner {
+    private final GoodInserter goodInserter;
+    private final RecipeInserter recipeInserter;
+    private final PopulationInserter populationInserter;
     private final BuildingInserter buildingInserter;
     private final Cost_Building_goodsInserter costBuildingGoodsInserter;
-    private final GoodInserter goodInserter;
-    private final PopulationInserter populationInserter;
     private final Population_RequirementInserter populationRequirementInserter;
     private final Cost_Building_PopulationInserter costBuildingPopulation;
 
 
     @Override
     public void run(ApplicationArguments args) throws Exception {
-        buildingInserter.run();
         goodInserter.run();
+        recipeInserter.run();
         populationInserter.run();
+        buildingInserter.run();
         costBuildingGoodsInserter.run();
         populationRequirementInserter.run();
         costBuildingPopulation.run();
