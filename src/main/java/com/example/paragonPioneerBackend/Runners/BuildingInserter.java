0d415cedc1705ebb82e29263d4fcc0ee9c997347
fGericke <florian.gericke@endava.com>
2024-01-05 17:13:58 +0100

    adding the recipes and production rates to building inserter

--- src/main/java/com/example/paragonPioneerBackend/Runners/BuildingInserter.java
+++ src/main/java/com/example/paragonPioneerBackend/Runners/BuildingInserter.java
@@ -3,26 +3,29 @@ package com.example.paragonPioneerBackend.Runners;
 import com.example.paragonPioneerBackend.Entity.PopulationBuilding;
 import com.example.paragonPioneerBackend.Entity.ProductionBuilding;
 import com.example.paragonPioneerBackend.Repository.BuildingRepository;
+import com.example.paragonPioneerBackend.Repository.RecipeRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
 @Component
 @RequiredArgsConstructor
-public class BuildingInserter{
+public class BuildingInserter {
     private final BuildingRepository repository;
-    private record Inserter(String name, String remarks) {
+    private final RecipeRepository recipeRepository;
+
+    private record Inserter(String name, String recipe, float productionPerMinute, String remarks) {
     }
 
     Inserter[] inserts = {
-            new Inserter("Lumberjack", ""),
-            new Inserter("Forester", ""),
-            new Inserter("Warehouse I", ""),
-            new Inserter("Well", ""),
-            new Inserter("Kontor I", "Limited to one per island"),
-            new Inserter("Fisherman", ""),
-            new Inserter("Sawmill", ""),
-            new Inserter("Potato Field", ""),
-            new Inserter("Potato Farm", "")
+            new Inserter("Lumberjack", "", 0, ""),
+            new Inserter("Forester", "", 0, ""),
+            new Inserter("Warehouse I", "", 0, ""),
+            new Inserter("Well", "", 0, ""),
+            new Inserter("Kontor I", "", 0, ",Limited to one per island"),
+            new Inserter("Fisherman", "Fish", 0.75f, ""),
+            new Inserter("Sawmill", "", 0, ""),
+            new Inserter("Potato Field", "", 0, ""),
+            new Inserter("Potato Farm", "", 0, "")
     };
 
     public void run() {
@@ -33,7 +36,7 @@ public class BuildingInserter{
         repository.save(PopulationBuilding.builder().name("Paragon's Residence").capacity(30).remarks("").build());
 
         for (Inserter insert : inserts) {
-            repository.save(ProductionBuilding.builder().name(insert.name).remarks(insert.remarks).build());
+            repository.save(ProductionBuilding.builder().name(insert.name).remarks(insert.remarks).recipe(recipeRepository.findByOutputNameIs(insert.recipe)).productionPerMinute(insert.productionPerMinute).build());
         }
     }
 }
