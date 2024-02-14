c2bd9c24b7dcab8ebb735ffe464d8935ed8f0ab9
fGericke <florian.gericke@endava.com>
2024-02-14 11:09:19 +0100

    add slug functions to the Building Entity

--- src/main/java/com/example/paragonPioneerBackend/Runners/BuildingInserter.java
+++ src/main/java/com/example/paragonPioneerBackend/Runners/BuildingInserter.java
@@ -1,9 +1,11 @@
 package com.example.paragonPioneerBackend.Runners;
 
-import com.example.paragonPioneerBackend.Entity.PopulationBuilding;
-import com.example.paragonPioneerBackend.Entity.ProductionBuilding;
-import com.example.paragonPioneerBackend.Repository.BuildingRepository;
-import com.example.paragonPioneerBackend.Repository.RecipeRepository;
+import com.example.paragonPioneerBackend.Dto.BuildingDTO;
+import com.example.paragonPioneerBackend.Dto.PopulationBuildingDTO;
+import com.example.paragonPioneerBackend.Dto.ProductionBuildingDTO;
+import com.example.paragonPioneerBackend.Service.BuildingService;
+import com.example.paragonPioneerBackend.Service.RecipeService;
+import com.example.paragonPioneerBackend.Util.OptionalUtil;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
@@ -13,8 +15,8 @@ import org.springframework.stereotype.Component;
 @Component
 @RequiredArgsConstructor
 public class BuildingInserter {
-    private final BuildingRepository repository;
-    private final RecipeRepository recipeRepository;
+    private final BuildingService<BuildingDTO> buildingService;
+    private final RecipeService recipeService;
 
     private record Inserter(String name, String recipe, float productionPerMinute, String remarks) {
     }
@@ -35,14 +37,20 @@ public class BuildingInserter {
      * Run the insertions
      */
     public void run() {
-        repository.save(PopulationBuilding.builder().name("Pioneer's Hut").capacity(10).remarks("").build());
-        repository.save(PopulationBuilding.builder().name("Colonist's House").capacity(15).remarks("").build());
-        repository.save(PopulationBuilding.builder().name("Townsmen's House").capacity(20).remarks("").build());
-        repository.save(PopulationBuilding.builder().name("Merchant's Mansion").capacity(25).remarks("").build());
-        repository.save(PopulationBuilding.builder().name("Paragon's Residence").capacity(30).remarks("").build());
+        buildingService.post(PopulationBuildingDTO.builder().name("Pioneer's Hut").capacity(10).remarks("").build());
+        buildingService.post(PopulationBuildingDTO.builder().name("Colonist's House").capacity(15).remarks("").build());
+        buildingService.post(PopulationBuildingDTO.builder().name("Townsmen's House").capacity(20).remarks("").build());
+        buildingService.post(PopulationBuildingDTO.builder().name("Merchant's Mansion").capacity(25).remarks("").build());
+        buildingService.post(PopulationBuildingDTO.builder().name("Paragon's Residence").capacity(30).remarks("").build());
 
         for (Inserter insert : inserts) {
-            repository.save(ProductionBuilding.builder().name(insert.name).remarks(insert.remarks).recipe(recipeRepository.findByOutputNameIs(insert.recipe)).productionPerMinute(insert.productionPerMinute).build());
+
+            buildingService.post(ProductionBuildingDTO.builder()
+                    .name(insert.name)
+                    .remarks(insert.remarks)
+                    .idOfRecipe(OptionalUtil.getIdOrEmpty(recipeService.findByName(insert.recipe)))
+                    .productionPerMinute(insert.productionPerMinute)
+                    .build());
         }
     }
 }
