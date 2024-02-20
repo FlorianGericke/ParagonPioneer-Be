5ad3199d739721100d40fc268362b0434ddf14df
fGericke <florian.gericke@endava.com>
2024-02-20 16:03:18 +0100

    adding Bean for authenticationProvider and passwordEncoder to the ApplicationConfig

--- src/main/java/com/example/paragonPioneerBackend/Bin/Config/ApplicationConfig.java
+++ src/main/java/com/example/paragonPioneerBackend/Bin/Config/ApplicationConfig.java
@@ -5,8 +5,12 @@ import com.example.paragonPioneerBackend.Repository.AdminRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 @RequiredArgsConstructor
@@ -17,5 +21,19 @@ public class ApplicationConfig {
     public UserDetailsService userDetailsService() {
         return username -> adminRepository.findByEmail(username)
                 .orElseThrow(() -> new UsernameNotFoundException("No Username found: " + username));
+    }
+
+    @Bean
+    public AuthenticationProvider authenticationProvider() {
+        DaoAuthenticationProvider authenticationProvider = new DaoAuthenticationProvider();
+        authenticationProvider.setUserDetailsService(userDetailsService());
+        authenticationProvider.setPasswordEncoder(passwordEncoder());
+
+        return authenticationProvider;
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 }
