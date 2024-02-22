ba318ca4c59ee9b8c58ed056f204b7f056809d45
fGericke <florian.gericke@endava.com>
2024-02-22 10:21:51 +0100

    adding DTOs for Auth

---
+++ src/main/java/com/example/paragonPioneerBackend/Bin/Auth/AuthenticationRequest.java
@@ -0,0 +1,15 @@
+package com.example.paragonPioneerBackend.Bin.Auth;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class AuthenticationRequest {
+    String email;
+    String password;
+}
