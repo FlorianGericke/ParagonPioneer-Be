a30016db01baefc1522e2b20001462ccc7750470
fGericke <florian.gericke@endava.com>
2024-02-22 10:23:59 +0100

    add Authentication Manager

--- src/main/java/com/example/paragonPioneerBackend/Bin/Config/ApplicationConfig.java
+++ src/main/java/com/example/paragonPioneerBackend/Bin/Config/ApplicationConfig.java
@@ -5,8 +5,10 @@ import com.example.paragonPioneerBackend.Repository.AdminRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -35,5 +37,10 @@ public class ApplicationConfig {
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
     }
 }
