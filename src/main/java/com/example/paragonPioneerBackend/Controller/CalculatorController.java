635141b874634642b8161dfbd62aa10eb25283ee
fGericke <florian.gericke@endava.com>
2024-03-30 14:07:53 +0100

    refactoring Calculator DTOs and Controller

--- src/main/java/com/example/paragonPioneerBackend/Controller/CalculatorController.java
+++ src/main/java/com/example/paragonPioneerBackend/Controller/CalculatorController.java
@@ -1,32 +1,36 @@
 package com.example.paragonPioneerBackend.Controller;
 
-import com.example.paragonPioneerBackend.Dto.CalculationOutputDTO;
+import com.example.paragonPioneerBackend.Dto.response.mappers.calculator.CalculationMapper;
 import com.example.paragonPioneerBackend.Service.CalculatorService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
 /**
- * This is a Rest Controller class named CalculatorController.
- * It handles the requests for calculations.
- * It uses the CalculatorService to perform the calculations.
+ * The CalculatorController class is a REST controller that handles requests to the /api/v1/calculator endpoint.
+ * It is annotated with @RestController to indicate that it is a Spring REST controller.
+ * It is annotated with @RequestMapping to map requests to the /api/v1/calculator endpoint to this controller.
  */
-@RequiredArgsConstructor
 @RestController
+@RequiredArgsConstructor
 @RequestMapping(path = "/api/v1/calculator")
 public class CalculatorController {
 
-    // The service used to perform calculations
+    // The CalculatorService that performs the calculations
     private final CalculatorService service;
+    // The CalculationMapper that maps CalculationResponses to CalculationMappers
+    private final CalculationMapper calculationMapper;
 
     /**
-     * This is a GET mapping that produces a JSON response.
-     * It takes a goodSlug as a request parameter and returns a CalculationOutputDTO.
-     * The CalculationOutputDTO is the result of the calculation performed by the service.
+     * Handles GET requests to the /api/v1/calculator/{goodSlug} endpoint.
+     * It uses the CalculatorService to calculate the result for the given goodSlug.
+     * It then uses the CalculationMapper to map the CalculationResponse to a CalculationMapper.
+     *
+     * @param goodSlug The slug of the good for which to calculate the result.
+     * @return A CalculationMapper representing the result of the calculation.
      */
-    @GetMapping(produces = "application/json")
-    public @ResponseBody CalculationOutputDTO putPopulationBuildingEntity(
-            @RequestParam(name = "goodSlug") String goodSlug
-    ) {
-        return  CalculationOutputDTO.createFromEntity(service.calculate(goodSlug));
+    @GetMapping(value = "/{goodSlug}", produces = "application/json")
+    public @ResponseBody CalculationMapper putPopulationBuildingEntity(
+            @PathVariable String goodSlug) {
+        return calculationMapper.map(service.calculate(goodSlug));
     }
 }
